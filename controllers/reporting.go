c2c3e7e6bdc7e0518dcb8d5b971e9e882b48d73d
Amanda <aeverett1317@gmail.com>
2019-04-01 10:19:03 -0700

    created route for users to request volunteer hour report

--- controllers/reporting.go
+++ controllers/reporting.go
@@ -21,6 +21,20 @@ type HoursWorkedByNPO struct {
 	HoursWorked int64
 }
 
+type HoursWorkedByEvent struct {
+	EventID        uint
+	EventName      string
+	EventStartTime int64
+	EventEndTime   int64
+	HoursWorked    int64
+}
+
+type HoursWorkedByTag struct {
+	TagID       uint
+	TagName     string
+	HoursWorked int64
+}
+
 func GetVolunteerHours(ctx iris.Context) {
 	db, err := databaseConfig.DbStart()
 	if err != nil {
@@ -37,40 +51,111 @@ func GetVolunteerHours(ctx iris.Context) {
 	var volunteerShifts []types.Shift
 	db.Table("shifts").Where("shifts.volunteer_id = ? AND shifts.actual_start_time >= ? AND shifts.actual_start_time <= ?", requestBody.VolunteerID, requestBody.StartDate, requestBody.EndDate).Find(&volunteerShifts)
 
-	//Get Hours worked by volunteer sorted by NPO
+	//Get Hours worked sorted by NPO
 	var hoursWorkedByNPO map[uint]HoursWorkedByNPO
+	hoursWorkedByNPO = make(map[uint]HoursWorkedByNPO)
+
+	//Acumulate hours worked sorted by Event
+	var hoursWorkedByEvent map[uint]HoursWorkedByEvent
+	hoursWorkedByEvent = make(map[uint]HoursWorkedByEvent)
+
+	//Accumulate hours worked sorked by Tag
+	var hoursWorkedByTag map[uint]HoursWorkedByTag
+	hoursWorkedByTag = make(map[uint]HoursWorkedByTag)
+
 	for _, shift := range volunteerShifts {
+		//Get Event info
 		var eventInfo types.Event
 		db.First(&eventInfo, shift.EventID)
+
+		//Get shift hours
+		shiftMinutes := (shift.ActualEndTime - shift.ActualStartTime) / 60000
+		shiftHours := shiftMinutes / 60
+
+		//Get NPO info
+		var npoInfo types.NPO
+		db.Select("npo_name").First(&npoInfo, eventInfo.NPOID)
+
+		//By NPO
 		_, ok := hoursWorkedByNPO[eventInfo.NPOID]
 		if ok {
 
-			var npoInfo types.NPO
-			db.Select("npo_name").First(&npoInfo, eventInfo.NPOID)
-			shiftMinutes := (shift.ActualEndTime - shift.ActualStartTime) / 60000
-			shiftHours := shiftMinutes / 60
 			updatedShiftHours := hoursWorkedByNPO[eventInfo.NPOID].HoursWorked + shiftHours
-			npoHoursWorked := HoursWorkedByNPO{
+			hoursWorkedByNPO[eventInfo.NPOID] = HoursWorkedByNPO{
 				NPOID:       eventInfo.NPOID,
 				NPOName:     npoInfo.NPOName,
 				HoursWorked: updatedShiftHours,
 			}
-			hoursWorkedByNPO[eventInfo.NPOID] = npoHoursWorked
 
 		} else {
-			var npoInfo types.NPO
-			db.Select("npo_name").First(&npoInfo, eventInfo.NPOID)
-			shiftMinutes := (shift.ActualEndTime - shift.ActualStartTime) / 60000
-			shiftHours := shiftMinutes / 60
-			npoHoursWorked := HoursWorkedByNPO{
+			hoursWorkedByNPO[eventInfo.NPOID] = HoursWorkedByNPO{
 				NPOID:       eventInfo.NPOID,
 				NPOName:     npoInfo.NPOName,
 				HoursWorked: shiftHours,
 			}
-			hoursWorkedByNPO[eventInfo.NPOID] = npoHoursWorked
 		}
+
+		//By Event
+		_, eventOK := hoursWorkedByEvent[eventInfo.ID]
+
+		if eventOK {
+			updatedShiftHours := hoursWorkedByEvent[eventInfo.ID].HoursWorked + shiftHours
+			hoursWorkedByEvent[eventInfo.ID] = HoursWorkedByEvent{
+				EventID:        eventInfo.ID,
+				EventName:      eventInfo.Name,
+				EventStartTime: eventInfo.StartTime,
+				EventEndTime:   eventInfo.EndTime,
+				HoursWorked:    updatedShiftHours,
+			}
+		} else {
+			hoursWorkedByEvent[eventInfo.ID] = HoursWorkedByEvent{
+				EventID:        eventInfo.ID,
+				EventName:      eventInfo.Name,
+				EventStartTime: eventInfo.StartTime,
+				EventEndTime:   eventInfo.EndTime,
+				HoursWorked:    shiftHours,
+			}
+		}
+
+		//By Tag
+
+		//Get Tags by event first
+		var eventTags []types.Tag
+		db.Table("tags").Joins("inner join event_tags on event_tags.tag_id = tags.id").Joins("inner join events on event_tags.event_id = events.id").Where("events.id = ?", eventInfo.ID).Find(&eventTags)
+		fmt.Println(eventTags)
+		for _, tag := range eventTags {
+			_, tagOK := hoursWorkedByTag[tag.ID]
+			if tagOK {
+				hoursWorkedByTag[tag.ID] = HoursWorkedByTag{
+					TagID:       tag.ID,
+					TagName:     tag.TagName,
+					HoursWorked: shiftHours,
+				}
+			} else {
+				updatedShiftHours := hoursWorkedByTag[eventInfo.ID].HoursWorked + shiftHours
+				hoursWorkedByTag[tag.ID] = HoursWorkedByTag{
+					TagID:       tag.ID,
+					TagName:     tag.TagName,
+					HoursWorked: updatedShiftHours,
+				}
+			}
+
+		}
+
 	}
-	fmt.Print(hoursWorkedByNPO)
-	ctx.JSON(hoursWorkedByNPO)
+
+	type ReportData struct {
+		HoursByNPO   map[uint]HoursWorkedByNPO
+		HoursByEvent map[uint]HoursWorkedByEvent
+		HoursByTag   map[uint]HoursWorkedByTag
+	}
+
+	reportData := ReportData{
+		HoursByNPO:   hoursWorkedByNPO,
+		HoursByEvent: hoursWorkedByEvent,
+		HoursByTag:   hoursWorkedByTag,
+	}
+
+	ctx.JSON(reportData)
 
 }
